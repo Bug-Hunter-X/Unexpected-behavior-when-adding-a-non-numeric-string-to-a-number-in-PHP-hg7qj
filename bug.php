2527d0496c6601d48d62fa44b5d60b4c1767912c
BugHunter-X <askzenai@gmail.com>
2025-01-02 19:52:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,13 @@
+```php
+function calculateSum($numbers) {
+  $sum = 0;
+  foreach ($numbers as $number) {
+    $sum += $number; 
+  }
+  return $sum;
+}
+
+$numbers = [1, 2, 3, '4a'];
+$sum = calculateSum($numbers);
+echo "Sum: " . $sum; //Notice unexpected result
+```
